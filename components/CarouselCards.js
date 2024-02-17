5b6fe3dc57270522c446d9b6e63c5d53079388d7
collinjung <kel.n7j@gmail.com>
2024-02-17 14:15:17 -0800

    added convex

--- components/CarouselCards.js
+++ components/CarouselCards.js
@@ -6,27 +6,11 @@ import CarouselCardItem, {
   ITEM_WIDTH,
 } from "../utils/CarouselCardItem";
 
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
 const CarouselCards = () => {
-  const data = [
-    {
-      title: "Veggie Plate",
-      body: "Pickup date: 2/14/2023. Pickup time: 8am",
-      info: "This plate contains cucumbers, eggs, tomato, cauliflower, and broccoli.",
-      imgUrl:
-        "https://www.shutterstock.com/image-vector/vector-illustration-fried-eggs-vegetables-600nw-1519043639.jpg",
-    },
-    {
-      title: "Plate 2",
-      body: "Pickup date: 2/14/2023. Pickup time: 2pm",
-      info: "testing testingSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-      imgUrl: "https://picsum.photos/id/10/200/300",
-    },
-    {
-      title: "Plate 3",
-      body: "Pickup date: 2/14/2023. Pickup time: 6pm",
-      imgUrl: "https://picsum.photos/id/12/200/300",
-    },
-  ];
+  const data = useQuery(api.myPlates.get);
   const [index, setIndex] = React.useState(0);
   const isCarousel = React.useRef(null);
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,11 +9,20 @@ import {
   Image,
 } from "react-native";
 import CarouselCards from "../components/CarouselCards";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import "react-native-get-random-values";
+import { CONVEX_URL } from "@env";
+
+const convex = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <CarouselCards />
+      <ConvexProvider client={convex}>
+        <CarouselCards />
+      </ConvexProvider>
     </SafeAreaView>
   );
 };
@@ -23,7 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#F8F8F8",
     alignItems: "center",
     justifyContent: "center",
-    // padding: 50,
   },
 });
 
